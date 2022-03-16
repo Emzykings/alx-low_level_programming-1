74afcbcd99edd43383428f8b58a33cb3e62e6f26
Emzykings <adeitanemmanuel086@gmail.com>
2022-03-16 16:57:13 -0700

    my first commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,6 @@
 
 
 void times_table(void)
-
 {
 
 int a = 0;
@@ -20,63 +19,33 @@ int b;
 int rep;
 
 while (a <= 9)
-
 {
-
 b = 0;
-
 while (b <= 9)
-
 {
-
 rep = a * b;
-
 if (b == 0)
-
 {
-
 _putchar('0' + rep);
-
 }
-
 else if (rep < 10)
-
 {
-
 _putchar(' ');
-
 _putchar('0' + rep);
-
 }
-
 else
-
 {
-
 _putchar('0' + rep / 10);
-
 _putchar('0' + rep % 10);
-
 }
-
 if (b < 9)
-
 {
-
 _putchar(',');
-
 _putchar(' ');
-
 }
-
 b++;
-
 }
-
 _putchar('\n');
-
 a++;
-
 }
-
 }
