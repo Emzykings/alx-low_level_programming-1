0dde6002b63c33701a79b60d6f7822d9add866a8
Emzykings <adeitanemmanuel086@gmail.com>
2022-03-16 16:05:21 -0700

    my first commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs  - Short description, single line
+ * @n: contians value
+ * Return: returns absolute value of n
+ */
+
+
+
+int _abs(int n)
+
+{
+
+if (n < 0)
+
+{
+
+n = (-1) * n;
+
+}
+
+return (n);
+
+}
